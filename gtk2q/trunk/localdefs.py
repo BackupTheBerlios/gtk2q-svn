8bb5f70f3cbd9b732f5e174eee4d8c951a38cddc
dannym <dannym@5a413ad5-a9f5-0310-9896-b8405c082a69>
2005-07-14 17:22:40 +0000

    add localdefs override

--- gtk2q/trunk/localdefs.py
+++ gtk2q/trunk/localdefs.py
@@ -1,2 +1,117 @@
 #!/usr/bin/env python
 
+# pstringtype = "UTF8String"
+# classprefix = "T"
+# enumprefix = "T"
+# interfaceprefix = "I"
+# interfaceunitprefix = "iu"
+# classunitprefix = "u"
+# wrapperpointerprefix = "PW"
+# wrapperprefix = "W"
+
+ptype1 = {
+}
+
+pusedclasses = { # from class implementation: list of classes used (implementation 'uses' clause for classes, interface 'uses' clause for interfaces)
+}
+
+psignalusedioc = {
+}
+
+psignalusediocimpl = {
+}
+
+poverridearraytypes = {
+}
+
+preturntransformers = {
+}
+
+nongobjectclasses = [
+]
+
+interfaceunitoverride = {
+}
+
+cclasses = [ # only a subset, mostly for properties of that type
+]
+
+c2pconstpointerparam = [ # 'const GdkColor*' as parameter
+]
+
+cclassconstructparams = {
+}
+
+c2ptypehash = {
+}
+
+c2penumcopied = [ # enums copied to delphi as-is
+]
+
+c2pstructcopied = { # simple structs copied to delphi as-is
+}
+
+c2pclassfunctions = { 
+}
+
+cskipsignals = [
+]
+
+cskipprops = [
+]
+
+forceexternals = [
+]
+
+paddmembervars = {
+}
+
+paddfuncs = {
+}
+
+paddprops = {
+}
+
+cskipfuncs = [
+]
+
+c2pcallbackpointers = {
+}
+
+c2pfuncparamoverride = {
+}
+
+c2psignalparamoverride = {
+}
+
+c2pproptypeoverride = {
+}
+
+c2pfuncparamnullable = {
+}
+
+#signalintf = ""
+#signalimpl = ""
+paddexternalgettype = [
+]
+
+externaltypemap = {
+}
+
+csignalhandlerargtype2definemap = {
+}
+
+underivable = [
+]
+
+classoverridables = {
+}
+
+pextraconstructcode = {
+}
+
+superclassoverride = {
+}
+
+csettypes = [
+]
