1e2ba82891ff99ff470eee7f1ce3521c35fd6471
dannym <dannym@5a413ad5-a9f5-0310-9896-b8405c082a69>
2005-07-17 16:09:19 +0000

    add interface unit

---
+++ gstq/trunk/static/iugst.pas
@@ -0,0 +1,25 @@
+unit iugst;
+
+interface
+uses ugtktypes, iugobject, sgstelement;
+
+const
+  GST_DUMMY = 7;
+{$DEFINE define_consts}
+{$INCLUDE static/gstincludes.inc}
+{$UNDEF define_consts}
+
+{$DEFINE define_types}
+{$INCLUDE static/gstincludes.inc}
+{$UNDEF define_types}
+
+type
+  {$INCLUDE objgtkcallback.inc}
+
+implementation
+
+{$DEFINE define_implementation}  
+{no $INCLUDE static/gstincludes.inc}
+{$UNDEF define_implementation}
+
+end.
