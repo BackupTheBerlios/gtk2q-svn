20a71a20645becbf3fc0361335aa868843ed4c96
dannym <dannym@5a413ad5-a9f5-0310-9896-b8405c082a69>
2006-01-31 19:54:50 +0000

    add some enums/flags

--- exoq/trunk/static/uexotypes.pas
+++ exoq/trunk/static/uexotypes.pas
@@ -1,6 +1,7 @@
 unit uexotypes;
 
 interface
+uses upangotypes;
 
 const
   // enumish, but string and extensible...
@@ -9,7 +10,12 @@ const
   CategoryMailClient = 'MailClient'
 
   // /usr/local/libexec/exo-helper-0.3 --launch <category>
-  
+
+type
+  TExoIconViewDropPosition = (ivNoDrop, ivDropInto, ivDropLeft, ivDropRight, ivDropAbove, ivDropBelow);
+  // TExoPangoEllipsizeMode = (peNone, peStart, peMiddle, peEnd); == TPangoEllipsizeMode
+  TExoToolbarsModelFlag = (tmNotRemovable, tmAcceptItemsOnly, tmOverrideStyle);
+  TExoToolbarsModelFlags = set of TExoToolbarsModelFlag;
 
 implementation
 
