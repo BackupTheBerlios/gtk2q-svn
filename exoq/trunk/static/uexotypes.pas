dffcda1b75b910fcab570c4dede22bf42654a66a
dannym <dannym@5a413ad5-a9f5-0310-9896-b8405c082a69>
2006-01-31 19:42:03 +0000

    add constants for categories and comment how to use it

--- exoq/trunk/static/uexotypes.pas
+++ exoq/trunk/static/uexotypes.pas
@@ -1,3 +1,16 @@
 unit uexotypes;
 
+interface
+
+const
+  // enumish, but string and extensible...
+  CategoryTerminalEmulator = 'TerminalEmulator';
+  CategoryWebBrowser = 'WebBrowser';
+  CategoryMailClient = 'MailClient'
+
+  // /usr/local/libexec/exo-helper-0.3 --launch <category>
+  
+
+implementation
+
 end.
