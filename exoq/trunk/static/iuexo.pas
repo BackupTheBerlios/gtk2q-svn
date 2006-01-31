4a1a228268af7be26c8166b68cf2fc9d92b42f8e
dannym <dannym@5a413ad5-a9f5-0310-9896-b8405c082a69>
2006-01-31 19:13:38 +0000

    add frontend units

---
+++ exoq/trunk/static/iuexo.pas
@@ -0,0 +1,48 @@
+unit iuexo;
+
+interface
+uses uexotypes, ugtktypes, iugtk, 
+  uexocellrendererellipsizedtext,
+  uexoellipsizedlabel,
+  uexoiconbar,
+  uexoiconview,
+  uexotoolbarseditordialog,
+  uexotoolbarseditor,
+  uexotoolbarsmodel,
+  uexotoolbarsview,
+  uexowraptable,
+  uexoxsessionclient,
+  sexocellrendererellipsizedtext,
+  sexoellipsizedlabel,
+  sexoiconbar,
+  sexoiconview,
+  sexotoolbarseditordialog,
+  sexotoolbarseditor,
+  sexotoolbarsmodel,
+  sexotoolbarsview,
+  sexowraptable,
+  sexoxsessionclient;
+
+const
+  EXO_DUMMY = 7;
+{$DEFINE define_consts}
+{$INCLUDE exoincludes.inc}
+{$UNDEF define_consts}
+
+{$DEFINE define_types}
+{$INCLUDE exoincludes.inc}
+{$UNDEF define_types}
+
+type
+  {$INCLUDE objexocallback.inc}
+
+implementation
+
+{$DEFINE define_implementation}
+{ $INCLUDE exoincludes.inc}
+{$UNDEF define_implementation}
+
+end.
+
+
+
